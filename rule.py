430c15cea7ea889df05aed0c14e59bce738145e1
Eginata Kasan <eginatakasan@gmail.com>
2020-02-07 16:54:25 +0700

    add print rule

--- rule.py
+++ rule.py
@@ -1,4 +1,4 @@
-class Rules:
+class RulesContainer:
     def __init__(self, tree):
         self.listOfRules = self.toRules(tree)
         
@@ -6,26 +6,36 @@ class Rules:
         if (tree.children):
             conditions = []
             for child in tree.children:
-                condition = Rule(tree.name, child[0])
+                condition = Condition(tree.name, child[0])
                 for childrenCondition in (self.toRules(child[1])):
                     childrenCondition.append(condition)
                     conditions.append(childrenCondition)
             return conditions
         else:
-            return [[Rule(tree.name)]]
+            return [[Condition(tree.name)]]
     
     def printRules(self):
         rulecopy = self.listOfRules.copy()
         for rules in rulecopy:
             ans = rules.pop(0).label
             print("if ",end="")
-            for rule in rules:
-                print(rule.label, end="=")
-                print(rule.value,end=", ")
+            for condition in rules:
+                print(condition.label, end="=")
+                print(condition.value, end="")
+                if (condition == rules[len(rules)-1]):
+                    print(" ", end="")
+                else:
+                    print(" ",end="and ")
             print("then ", end="")
             print(ans)
 
-class Rule:
+class Condition:
     def __init__(self,label,value=""):
         self.label = label
         self.value = value
+    
+    def __str__(self):
+        return (self.label + "=" + str(self.value))
+    
+    def __repr__(self):
+        return (self.label + "=" + str(self.value))
