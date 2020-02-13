a93ce37a4e4ce517ae6940ecaf20a2ccba32cbbd
Eginata Kasan <13517030@std.stei.itb.ac.id>
2020-02-13 22:14:45 +0700

    hapus komentar, mau dikumpul ini

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from id3 import DecisionTreeLearning
 
-dtl = DecisionTreeLearning("iris", "species", isPrune=True)
+dtl = DecisionTreeLearning("tennis", "play", isPrune=True)
 dtl.build()
 dtl.printTree()
 
--- tree.py
+++ tree.py
@@ -20,35 +20,3 @@ class Tree:
 
     def __repr__(self):
             return '<tree node representation>'
-        
-# tree = Tree('afd')
-# treeb = Tree('bfdsaf')
-# treec = Tree('cdasdas')
-# treed = Tree('ddasdas')
-# treee = Tree('edasdas')
-# treef = Tree('fdasdas')
-# treeg = Tree('gdasdas')
-# treeh = Tree('hdasdas')
-# treei = Tree('i')
-# treej = Tree('j')
-# treek = Tree('k')
-# treel = Tree('l')
-# treem = Tree('m')
-# treen = Tree('n')
-# treeo = Tree('o')
-# treep = Tree('p')
-# treeq = Tree('q')
-
-# tree.addChild('b-val',treeb)
-# tree.addChild('c-val',treec)
-# treeb.addChild('d-val',treed)
-# treec.addChild('f-val',treef)
-# treec.addChild('g-val',treeg)
-# treec.addChild('h-val',treeh)
-# treed.addChild('i-val',treei)
-# tree.addChild('wakgeng', treej)
-# tree.addChild('coba', treek)
-# treej.addChild('cobalagi', treel)
-# treeb.addChild('cobaocba', treem)
-
-# print(tree)
